b750f08af5ad8dcf5e154288e34e0ffdb943f3b9
sakshi mane <manesakshishivaji2006@gmail.com>
2026-02-27 12:37:59 +0530

    Add files via upload

---
+++ stackop.java
@@ -0,0 +1,74 @@
+interface Stack {
+    int size = 5;
+
+    void push(int data);
+    void pop();
+    void display();
+    void overflow();
+    void underflow();
+}
+
+class IntegerStack implements Stack {
+    int arr[] = new int[size];
+    int top = -1;
+
+    public void push(int data) {
+        if (top == size - 1) {
+            overflow();
+        } else {
+            arr[++top] = data;
+            System.out.println(data + " inserted");
+        }
+    }
+
+    public void pop() {
+        if (top == -1) {
+            underflow();
+        } else {
+            System.out.println(arr[top--] + " removed");
+        }
+    }
+
+    public void display() {
+        if (top == -1) {
+            System.out.println("Stack is empty");
+        } else {
+            System.out.println("Stack elements:");
+            for (int i = top; i >= 0; i--) {
+                System.out.println(arr[i]);
+            }
+        }
+    }
+
+    public void overflow() {
+        System.out.println("Stack Overflow");
+    }
+
+    public void underflow() {
+        System.out.println("Stack Underflow");
+    }
+}
+
+class TestStack {
+    public static void main(String[] args) {
+        IntegerStack s = new IntegerStack();
+
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        s.push(40);
+        s.push(50);
+        s.push(60); // overflow
+
+        s.display();
+
+        s.pop();
+        s.pop();
+        s.pop();
+        s.pop();
+        s.pop();
+        s.pop(); // underflow
+
+        s.display();
+    }
+}
---
+++ studentresult.java
@@ -0,0 +1,48 @@
+class Student {
+    int rollNo;
+
+    void setRollNo(int r) {
+        rollNo = r;
+    }
+
+    int getRollNo() {
+        return rollNo;
+    }
+}
+
+class Test extends Student {
+    int sub1, sub2;
+
+    void setMarks(int m1, int m2) {
+        sub1 = m1;
+        sub2 = m2;
+    }
+
+    void getMarks() {
+        System.out.println("Marks in Subject 1: " + sub1);
+        System.out.println("Marks in Subject 2: " + sub2);
+    }
+}
+
+interface Sports {
+    int sMarks = 20; // sports marks
+    void set();
+}
+
+class Result extends Test implements Sports {
+
+    public void set() {
+        System.out.println("Sports Marks: " + sMarks);
+    }
+
+    void displayResult() {
+        int total = sub1 + sub2 + sMarks;
+        double percentage = total / 3.0;
+
+        System.out.println("Roll No: " + getRollNo());
+        getMarks();
+        set();
+        System.out.println("Total Marks: " + total);
+        System.out.println("Percentage: " + percentage);
+    }
+}
